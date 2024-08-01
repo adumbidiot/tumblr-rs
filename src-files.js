879f4401b60c6b043db91ae161d14a4e2b4425a7
adumbidiot <adumbidiot@users.noreply.github.com>
2024-08-01 20:26:04 +0000

    deploy: 19db5c3ab8b506f0035ac90bbd028f0b88b522f7

---
+++ src-files.js
@@ -0,0 +1,5 @@
+var srcIndex = new Map(JSON.parse('[\
+["tumblr",["",[["model",[],["content_block.rs","initial_state.rs"]]],["client.rs","lib.rs","model.rs"]]],\
+["tumblr_cli",["",[["commands",[],["download.rs"]]],["commands.rs","main.rs"]]]\
+]'));
+createSrcSidebar();
---
+++ trait.impl/argh/trait.TopLevelCommand.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"tumblr_cli":[["impl TopLevelCommand for <a class=\"struct\" href=\"tumblr_cli/struct.Options.html\" title=\"struct tumblr_cli::Options\">Options</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
