3ea9feda5ec17ace6e4344cdf0edfaa285e87d9e
adumbidiot <adumbidiot@users.noreply.github.com>
2024-10-02 18:22:25 +0000

    deploy: 53a89699790f9aa5067ae23587d175977061cad1

---
+++ trait.impl/argh/trait.SubCommand.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"tumblr_cli":[["impl SubCommand for <a class=\"struct\" href=\"tumblr_cli/commands/download/struct.Options.html\" title=\"struct tumblr_cli::commands::download::Options\">Options</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
